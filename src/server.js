8b2f1594487e45d80f49817d40c0071e2e862620
Guilherme Girardi <guilherme@ardanis.com>
2024-02-03 14:33:58 -0300

    Set dynamic POST

--- src/server.js
+++ src/server.js
@@ -5,9 +5,21 @@ const users = []
 
 // request => Readable Stream
 // response => Writable Stream
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request
-  
+
+  const buffers = []
+
+  for await (const chunk of request) {
+    buffers.push(chunk)
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    request.body = null
+  }
+
   if(method === 'GET' && url === '/users') {
     return response
       .setHeader('Content-type', 'application/json')
@@ -15,10 +27,12 @@ const server = http.createServer((request, response) => {
   }
 
   if(method === 'POST' && url === '/users') {
+    const { name, email } = request.body
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "john@john.com"
+      name,
+      email,
     })
 
     return response.writeHead(201).end()
--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -1,16 +1,4 @@
 import http from 'node:http'
-import { Transform } from 'node:stream'
-
-class InverseNumberString extends Transform {
-  _transform(chunk, encoding, callback) {
-    const transformed = Number(chunk.toString()) * -1
-
-    console.log('Here transformed:', transformed)
-
-    // callback(handle the error, transformed data, )
-    callback(null, Buffer.from(String(transformed)))
-  }
-}
 
 // request => Readable Stream
 // response => Writable Stream
@@ -23,12 +11,7 @@ const server = http.createServer(async (request, response) => {
 
   const fullStreamContent = Buffer.concat(buffers).toString()
 
-  console.log(fullStreamContent)
-
   return response.end(fullStreamContent)
-  // return request
-  //   .pipe(new InverseNumberString())
-  //   .pipe(response)
 })
 
 server.listen(3334)
