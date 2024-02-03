08fe2f309ebc52f2be7b6385d13837670f58be51
Guilherme Girardi <guilherme@ardanis.com>
2024-02-03 14:39:12 -0300

    buffer

---
+++ streams/buffer.js
@@ -0,0 +1,6 @@
+// Represent a space in the computers memory. 
+// Used to transit data very fast.
+// Read and Write data in the computers memory.
+const buf = Buffer.from("ok")
+
+console.log(buf.toJSON())
